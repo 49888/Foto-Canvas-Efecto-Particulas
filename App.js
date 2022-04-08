6b8464e795fedc735a41fcf56a2897f06f812089
DESKTOP-UJ53SU5\1812 <francogadea1812@gmail.com>
2022-04-07 21:11:12 -0300

    Agregamos la posibilidad de Redimencionar las Fotos usando: Cropper.js
    Cambiamos el contenerdor del canvas de div a main
    
    Cambiamos las dimensiones del Contenedor del canvas
    
    Todo parece funcionar correctamente

--- App.js
+++ App.js
@@ -13,11 +13,9 @@ const $File = document.getElementById('File');
 const $Select = document.getElementById('Select');
 const $Color = document.getElementById('Color');
 
-const $CanvasContainer = document.getElementById('Canvas-container');
+const $Main = document.getElementById('Main-container');
 
 const $Stop = document.getElementById('Stop');
-
-
 
 $Stop.addEventListener('click', ()=>{
 
@@ -27,26 +25,33 @@ $Stop.addEventListener('click', ()=>{
 
 $File.addEventListener('change', async function(e){
 
-    let files = e.target.files;
-
     stopAnimation();
 
-    $CanvasContainer.innerHTML = "";
+    $Main.innerHTML = '';
 
-        let canvas = document.createElement('canvas');
-
-        canvas.width = 500; canvas.height = 600; canvas.classList.add('rounded-2');
-
-    $CanvasContainer.appendChild(canvas);
+    let files = e.target.files;
 
     try {
 
         //Read photo
-        let result = await readFileAsDataURL(files[0]);
+        let url = await readFileAsDataURL(files[0]);
 
+
+        //Resize
+        let result = await Resize(url, $Main);
         
+        $Main.innerHTML = '';
+        
+        let canvas = document.createElement('canvas');
 
-        //Animation particles
+        canvas.width = 500; canvas.height = 750; 
+        canvas.classList.add('rounded-2');
+        canvas.style.display = 'block'; 
+        canvas.style.margin = 'auto';
+
+        $Main.appendChild(canvas);
+        
+        //Start Animation particles
         switch($Select.value){
             
             //Efecto 1
@@ -64,6 +69,7 @@ $File.addEventListener('change', async function(e){
                 particles(canvas, result, {useMap: true, useMapColor: true});
                 break;        
         }
+        //*/
 
 
     } catch (error) {
@@ -71,4 +77,41 @@ $File.addEventListener('change', async function(e){
         console.log(error);
     }
 })
+
+
+
+function Resize(url, component){
+
+    
+    return new Promise((resolve, reject)=>{
+
+        let template = document.getElementById('resize-template');
+        let $resize = template.content.cloneNode(true);
+
+        
+        let img = $resize.querySelector('#Img');
+        img.src = url;
+        
+        //Cropper JS
+            let cropper = new Cropper(img, {aspectRatio: 2 / 3});//<---
+
+
+        $resize.querySelector('#resize').addEventListener('click', (e)=>{
+
+            let canvas = cropper.getCroppedCanvas({width: 500, height: 750});//<---
+
+            
+            resolve(canvas.toDataURL());//<---
+        });
+
+        $resize.querySelector('#cancel').addEventListener('click', (e)=>{
+
+            component.innerHTML = '';
+
+            reject('Has cancelado el redimensionado de la foto');
+        });
+
+        component.appendChild($resize);
+    })
+}
 
